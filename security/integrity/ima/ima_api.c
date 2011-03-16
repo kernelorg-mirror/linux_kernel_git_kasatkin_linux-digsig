104ae071c54b57f3dae8fa8b6561d4d0de152b05
Dmitry Kasatkin <dmitry.kasatkin@nokia.com>
2011-03-15 20:41:57 -0400

    ima: add support for different security.ima data types
    IMA currently verifies the integrity of a file based on a known 'good'
    measurement value.  We anticipate other methods for verifying file data
    integrity.  This patch reserves the first byte of 'security.ima' as a
    place holder for the type of method.
    
    Signed-off-by: Dmitry Kasatkin <dmitry.kasatkin@nokia.com>
    Signed-off-by: Mimi Zohar <zohar@linux.vnet.ibm.com>

--- security/integrity/ima/ima_api.c
+++ security/integrity/ima/ima_api.c
@@ -150,8 +150,9 @@ int ima_collect_measurement(struct integrity_iint_cache *iint,
 	if (!(iint->flags & IMA_COLLECTED)) {
 		u64 i_version = file->f_dentry->d_inode->i_version;
 
-		memset(iint->digest, 0, IMA_DIGEST_SIZE);
-		result = ima_calc_hash(file, iint->digest);
+		memset(iint->digest, 0, IMA_DIGEST_SIZE + 1);
+		iint->digest[0] = IMA_XATTR_DIGEST;
+		result = ima_calc_hash(file, iint->digest + 1);
 		if (!result) {
 			iint->version = i_version;
 			iint->flags |= IMA_COLLECTED;
@@ -195,7 +196,7 @@ void ima_store_measurement(struct integrity_iint_cache *iint,
 		return;
 	}
 	memset(&entry->template, 0, sizeof(entry->template));
-	memcpy(entry->template.digest, iint->digest, IMA_DIGEST_SIZE);
+	memcpy(entry->template.digest, iint->digest + 1, IMA_DIGEST_SIZE);
 	strncpy(entry->template.file_name, filename, IMA_EVENT_NAME_LEN_MAX);
 
 	result = ima_store_template(entry, violation, inode);
