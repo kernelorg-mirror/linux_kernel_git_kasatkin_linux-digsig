dc94f97eb61419793626224f5e05ffcd401b7863
Dmitry Kasatkin <d.kasatkin@samsung.com>
2013-11-08 16:27:18 +0200

    ima: export ima_alloc_tfm and ima_free_tfm
    Export ima_alloc_tfm() and ima_free_tfm() to be used by directory
    verification code.
    
    Signed-off-by: Dmitry Kasatkin <d.kasatkin@samsung.com>

--- security/integrity/ima/ima_crypto.c
+++ security/integrity/ima/ima_crypto.c
@@ -111,7 +111,7 @@ int __init ima_init_crypto(void)
 	return 0;
 }
 
-static struct crypto_shash *ima_alloc_tfm(enum hash_algo algo)
+struct crypto_shash *ima_alloc_tfm(enum hash_algo algo)
 {
 	struct crypto_shash *tfm = ima_shash_tfm;
 	int rc;
@@ -130,7 +130,7 @@ static struct crypto_shash *ima_alloc_tfm(enum hash_algo algo)
 	return tfm;
 }
 
-static void ima_free_tfm(struct crypto_shash *tfm)
+void ima_free_tfm(struct crypto_shash *tfm)
 {
 	if (tfm != ima_shash_tfm)
 		crypto_free_shash(tfm);
