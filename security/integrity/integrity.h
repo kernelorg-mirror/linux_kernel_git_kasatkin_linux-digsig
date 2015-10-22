tizenorg/profile.common.kernel-common

ima: provide 'appraise_action=log' policy parameter
It may be a necessary to enable appraisal but run it in permissive mode.
Appraisal result can be added to the measurement list or for auditing,
but system needs to continue running.

This patch provide 'appraise_action=log' option, which allows to ignore
appraisal result.

Signed-off-by: Dmitry Kasatkin <d.kasatkin@samsung.com>

security/integrity/integrity.h
@@ -31,6 +31,7 @@
 #define IMA_DIGSIG_REQUIRED	0x01000000
 #define IMA_PERMIT_DIRECTIO	0x02000000
 #define IMA_NEW_FILE		0x04000000
+#define IMA_APPRAISE_PERMIT	0x08000000
 
 #define IMA_DO_MASK		(IMA_MEASURE | IMA_APPRAISE | IMA_AUDIT | \
 				 IMA_APPRAISE_SUBMASK)

kernelorg-mirror/linux_kernel_git_kasatkin_linux-digsig

security/integrity/integrity.h
@@ -33,6 +33,7 @@
 #define IMA_NEW_FILE		0x04000000
 #define EVM_DIGSIG_REQUIRED	0x08000000
 #define IMA_DROP_CAPS		0x10000000
+#define IMA_APPRAISE_PERMIT	0x20000000
 
 #define IMA_DO_MASK		(IMA_MEASURE | IMA_APPRAISE | IMA_AUDIT | \
 				 IMA_APPRAISE_SUBMASK)